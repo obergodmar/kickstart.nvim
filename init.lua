4f649972f0d25e4dc2d47e3d53a713c65eed8e82
Vitaly <obergodmar@gmail.com>
2023-04-08 00:57:09 +0300

    feat: add php syntax highliting for treesitter

--- init.lua
+++ init.lua
@@ -204,7 +204,7 @@ pcall(require('telescope').load_extension, 'fzf')
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'css', 'tsx', 'typescript', 'help', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'css', 'tsx', 'typescript', 'php', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
