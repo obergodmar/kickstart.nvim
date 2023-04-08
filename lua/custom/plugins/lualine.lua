11583c8680e428f45f00bf0a9f5af5e3128723e9
Vitaly <obergodmar@gmail.com>
2023-04-08 23:25:26 +0300

    feat: configure lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -9,5 +9,29 @@ return {
 			component_separators = '|',
 			section_separators = '',
 		},
+		sections = {
+			lualine_a = { 'mode' },
+			lualine_b = { 'branch', 'diff', 'diagnostics' },
+			lualine_c = { 'filename', 'filesize' },
+			lualine_x = { 'encoding', 'fileformat', 'filetype' },
+			lualine_y = { 'selectioncount', 'searchcount', 'progress' },
+			lualine_z = { 'location' }
+		},
+		inactive_sections = {
+			lualine_a = { {
+				'windows',
+				mode = 1,
+				use_mode_colors = true,
+			} },
+			lualine_b = {},
+			lualine_c = { 'filename', 'filesize' },
+			lualine_x = { 'location' },
+			lualine_y = {},
+			lualine_z = {}
+		},
+		tabline = {},
+		winbar = {},
+		inactive_winbar = {},
+		extensions = {}
 	},
 }
