df6adc9b0b90881aa97251431133188efce3bc00
Vitaly <obergodmar@gmail.com>
2023-04-15 02:40:38 +0300

    feat: add new languages to treesitter

--- init.lua
+++ init.lua
@@ -204,8 +204,8 @@ pcall(require('telescope').load_extension, 'fzf')
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'css', 'tsx', 'typescript', 'javascript', 'php',
-    'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'css', 'tsx', 'typescript', 'javascript', 'graphql',
+    'html', 'sql', 'bash', 'php', 'ini', 'dockerfile', 'gitattributes', 'gitignore', 'vim', 'json', 'json5', 'markdown' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
