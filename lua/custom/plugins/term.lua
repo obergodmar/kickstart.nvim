35ee6482b04ef02937ddbc985e3c9b0a08f4d06d
Vitaly <obergodmar@gmail.com>
2023-04-17 01:40:27 +0300

    fix: change shell just in toggle term

--- lua/custom/plugins/term.lua
+++ lua/custom/plugins/term.lua
@@ -2,26 +2,27 @@ return {
 	'akinsho/toggleterm.nvim',
 	version = "*",
 	config = function()
-		local powershell_options = {
-			shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-			shellcmdflag =
-			"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-			shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-			shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-			shellquote = "",
-			shellxquote = "",
-		}
+		-- local powershell_options = {
+		-- 	shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+		-- 	shellcmdflag =
+		-- 	"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+		-- 	shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+		-- 	shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+		-- 	shellquote = "",
+		-- 	shellxquote = "",
+		-- }
 
-		if vim.fn.has("win32") == 1 then
-			for option, value in pairs(powershell_options) do
-				vim.opt[option] = value
-			end
-		end
-
+		-- if vim.fn.has("win32") == 1 then
+		-- 	for option, value in pairs(powershell_options) do
+		-- 		vim.opt[option] = value
+		-- 	end
+		-- end
+		--
 		require('toggleterm').setup {
 			shade_terminals = false,
 			start_in_insert = true,
 			open_mapping = [[<c-\>]],
+			shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
 		}
 	end
 }
