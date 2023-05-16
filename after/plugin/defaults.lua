ca01f4084b119a5645dece5e0f7e4f29b8ed2984
Vitaliy Kogogin <v.kogogin@vk.team>
2023-05-16 21:31:21 +0300

    feat: add automatic toggling between line number modes

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -118,3 +118,19 @@ vim.api.nvim_create_autocmd({ 'BufWinEnter', 'FileType' }, {
   group    = vim.api.nvim_create_augroup('nvim-lastplace', {}),
   callback = run
 })
+
+local numbertoggle = vim.api.nvim_create_augroup('numbertoggle', {})
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'WinEnter' }, {
+  group = numbertoggle,
+  callback = function()
+    vim.cmd [[if &nu && mode() != "i" | set rnu | endif]]
+  end
+})
+
+vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'WinLeave' }, {
+  group = numbertoggle,
+  callback = function()
+    vim.cmd [[if &nu | set nornu | endif]]
+  end
+})
