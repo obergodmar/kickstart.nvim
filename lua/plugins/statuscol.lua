aa2c44f533a99f5383ddc9e428b0012f1feae347
Vitaly <obergodmar@gmail.com>
2023-12-10 19:02:16 +0300

    feat: use correct version of statuscol

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -1,7 +1,8 @@
 ---@type LazyPluginSpec
 local P = {
   -- Status column plugin that provides a configurable 'statuscolumn' and click handlers. Requires Neovim >= 0.9.
-  'obergodmar/statuscol.nvim',
+  'luukvbaal/statuscol.nvim',
+  branch = vim.fn.has('nvim-0.10') and '0.10' or 'main',
   event = 'VeryLazy',
   config = function()
     local builtin = require('statuscol.builtin')
