0a74387f0431a10c91ac9485d74b7fca453f0db1
Vitaly <obergodmar@gmail.com>
2023-12-11 16:24:30 +0300

    feat: try lsp timeout

---
+++ lua/plugins/lsp-timeout.lua
@@ -0,0 +1,20 @@
+---@type LazyPluginSpec
+local P = {
+  'hinell/lsp-timeout.nvim',
+  init = function()
+    vim.g.lspTimeoutConfig = {
+      stopTimeout = 1000 * 60 * 5, -- ms, timeout before stopping all LSPs
+      startTimeout = 1000 * 10, -- ms, timeout before restart
+      silent = true, -- true to suppress notifications
+      filetypes = {
+        ignore = {
+          -- filetypes to ignore; empty by default
+          -- lsp-timeout is disabled completely
+          -- for these filetypes
+        },
+      },
+    }
+  end,
+}
+
+return P
