d8a1f03df367f59db7f3ddc5fa8b44de7bc9a158
Vitaly <obergodmar@gmail.com>
2024-04-30 01:05:38 +0300

    feat: add markdown preview plugin

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,12 @@
+---@type LazyPluginSpec
+local P = {
+  'obergodmar/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  build = 'cd app && yarn install',
+  init = function()
+    vim.g.mkdp_filetypes = { 'markdown' }
+  end,
+  ft = { 'markdown' },
+}
+
+return P
