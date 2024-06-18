5db3c6e2974ec3abc4de486b28c3342a5fc8d7bc
Vitaly <obergodmar@gmail.com>
2024-06-18 14:08:05 +0300

    feat: return back treesitter-textobjects

--- lua/core/treesitter.lua
+++ lua/core/treesitter.lua
@@ -4,6 +4,7 @@ local P = {
   'obergodmar/nvim-treesitter',
   dependencies = {
     'obergodmar/nvim-treesitter-context',
+    'obergodmar/nvim-treesitter-textobjects',
   },
   branch = 'master',
   config = function()
@@ -26,6 +27,42 @@ local P = {
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
+      textobjects = {
+        lsp_interop = {
+          enable = false,
+        },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            [']m'] = '@function.outer',
+            [']]'] = { query = '@class.outer', desc = 'Next class start' },
+
+            [']o'] = '@loop.*',
+
+            [']s'] = { query = '@scope', query_group = 'locals', desc = 'Next scope' },
+            [']z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
+          },
+          goto_next_end = {
+            [']M'] = '@function.outer',
+            [']['] = '@class.outer',
+          },
+          goto_previous_start = {
+            ['[m'] = '@function.outer',
+            ['[['] = '@class.outer',
+          },
+          goto_previous_end = {
+            ['[M'] = '@function.outer',
+            ['[]'] = '@class.outer',
+          },
+          goto_next = {
+            [']e'] = '@conditional.outer',
+          },
+          goto_previous = {
+            ['[e'] = '@conditional.outer',
+          },
+        },
+      },
     })
   end,
 }
