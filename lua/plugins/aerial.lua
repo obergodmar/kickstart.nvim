1dfe2f2f692b8041f32b9a705ff30adbf5ba9306
Vitaly <obergodmar@gmail.com>
2024-08-12 18:17:43 +0300

    fix: configure aerial

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -3,13 +3,29 @@ local P = {
   'obergodmar/aerial.nvim',
   config = function()
     require('aerial').setup({
+      layout = {
+        max_width = { 50, 0.3 },
+        width = nil,
+        min_width = 30,
+        placement = 'edge',
+      },
+      highlight_on_hover = true,
+      open_automatic = false,
       on_attach = function(bufnr)
         vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
         vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
       end,
     })
-    vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle!<CR>')
   end,
+  keys = {
+    {
+      '<leader>a',
+      '<cmd>AerialToggle!<CR>',
+      id = 'aerial_open',
+      desc = '[A]erial Open',
+      mode = 'n',
+    },
+  },
 }
 
 return P
