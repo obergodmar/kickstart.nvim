ff11c0add1a631fa7b4d0571d984a0944e9afe69
Vitaly (wsl) <obergodmar@gmail.com>
2024-08-18 03:38:50 +0300

    feat: add descriptions to gitsigns & add keymap for diffview

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -33,7 +33,7 @@ local P = {
           gs.next_hunk()
         end)
         return '<Ignore>'
-      end, { expr = true })
+      end, { expr = true, desc = "[gitsigns] Next Hunk" })
 
       map('n', '[c', function()
         if vim.wo.diff then
@@ -43,26 +43,26 @@ local P = {
           gs.prev_hunk()
         end)
         return '<Ignore>'
-      end, { expr = true })
+      end, { expr = true, desc = "[gitsigns] Prev Hunk" })
 
       -- Actions
-      map('n', '<leader>hs', gs.stage_hunk)
-      map('n', '<leader>hr', gs.reset_hunk)
+      map('n', '<leader>hs', gs.stage_hunk, { desc = '[gitsigns] Stage Hunk' })
+      map('n', '<leader>hr', gs.reset_hunk, { desc = '[gitsigns] Reset Hunk' })
       map('v', '<leader>hs', function()
         gs.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-      end)
+      end, { desc = '[gitsigns] Stage Hunk' })
       map('v', '<leader>hr', function()
         gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-      end)
-      map('n', '<leader>hS', gs.stage_buffer)
-      map('n', '<leader>hu', gs.undo_stage_hunk)
-      map('n', '<leader>hR', gs.reset_buffer)
-      map('n', '<leader>hp', gs.preview_hunk)
+      end, { desc = '[gitsigns] Reset Hunk' })
+      map('n', '<leader>hS', gs.stage_buffer, { desc = '[gitsigns] Stage Buffer' })
+      map('n', '<leader>hu', gs.undo_stage_hunk, { desc = '[gitsigns] Undo Stage Hunk' })
+      map('n', '<leader>hR', gs.reset_buffer, { desc = '[gitsigns] Reset Buffer' })
+      map('n', '<leader>hp', gs.preview_hunk, { desc = '[gitsigns] Preview Hunk' })
       map('n', '<leader>hb', function()
         gs.blame_line({ full = true })
-      end)
-      map('n', '<leader>hd', gs.diffthis)
-      map('n', '<leader>td', gs.toggle_deleted)
+      end, { desc = '[gitsigns] Blame Line' })
+      map('n', '<leader>hd', gs.diffthis, { desc = '[gitsigns] Diffthis' })
+      map('n', '<leader>td', gs.toggle_deleted, { desc = '[gitsigns] Toggle Deleted' })
     end,
   },
 }
