a25ea1ad505a3682e00ac6b643eacb94cc14365d
Vitalii Kogogin <vitalii.kogogin@celer-tech.com>
2024-12-23 13:11:48 +0300

    fix: do not save toggle term when saving auto session

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -9,6 +9,25 @@ local P = {
   ---@module "auto-session"
   ---@type AutoSession.Config
   opts = {
+    pre_save_cmds = {
+      function()
+        -- list current buffers
+        local buffers = vim.api.nvim_list_bufs()
+
+        local toggleterm_exists = false
+        for _, buf in ipairs(buffers) do
+          local buf_name = vim.api.nvim_buf_get_name(buf)
+          if buf_name:find('toggleterm#') then
+            toggleterm_exists = true
+            break
+          end
+        end
+
+        if toggleterm_exists then
+          vim.cmd([[ ToggleTermToggleAll ]])
+        end
+      end,
+    },
     cwd_change_handling = true,
     -- log_level = 'debug',
   },
